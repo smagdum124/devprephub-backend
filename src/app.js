7c406b4b0e99f30ce58813dfe6f29413fb8df08b
Magdum Shaikh <smagdum124@gmail.com>
2026-07-02 11:31:19 +0530

    Added newsletter subscription section in footer

--- src/app.js
+++ src/app.js
@@ -10,13 +10,15 @@ import newsletterRoutes from "./routes/newsletterRoutes.js";
 
 const app = express();
 
-app.use(cors({
-    origin: [
-        "http://localhost:5173",
-        "https://devprephub-nine.vercel.app",
-    ],
-    credentials: true,
-}));
+app.use(
+    cors({
+        origin: [
+            "http://localhost:5173",
+            "https://devprephub-nine.vercel.app",
+        ],
+        credentials: true,
+    })
+);
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
